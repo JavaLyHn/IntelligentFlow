3de4ef2ea2d9fee92d748b9eae3a9a05db55c437
JavaLyHn <2443560753@qq.com>
2026-03-08 18:28:19 +0800

    engine核心类开发

---
+++ core-workflow-java/src/main/java/com/lyhn/coreworkflowjava/workflow/engine/VariablePool.java
@@ -0,0 +1,9 @@
+package com.lyhn.coreworkflowjava.workflow.engine;
+
+import lombok.extern.slf4j.Slf4j;
+
+// 负责管理工作流执行过程中，节点之间的数据存储和引用，（一个 Node 引用其他 Node 的输出，靠的就是变量池）
+@Slf4j
+public class VariablePool {
+
+}
---
+++ core-workflow-java/src/main/java/com/lyhn/coreworkflowjava/workflow/engine/WorkflowEngine.java
@@ -0,0 +1,13 @@
+package com.lyhn.coreworkflowjava.workflow.engine;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+/**
+ * 负责调度和执行链路
+ */
+@Slf4j
+@Component
+public class WorkflowEngine {
+
+}
