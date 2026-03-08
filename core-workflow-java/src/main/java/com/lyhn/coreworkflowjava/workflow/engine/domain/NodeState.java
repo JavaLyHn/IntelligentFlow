3ac8ab606c0b17d342036ebc97ed5e1a0709f0d3
JavaLyHn <2443560753@qq.com>
2026-03-08 19:18:26 +0800

    工作流的数据结构开发

---
+++ core-workflow-java/src/main/java/com/lyhn/coreworkflowjava/workflow/engine/domain/NodeState.java
@@ -0,0 +1,10 @@
+package com.lyhn.coreworkflowjava.workflow.engine.domain;
+
+import com.lyhn.coreworkflowjava.workflow.engine.VariablePool;
+import com.lyhn.coreworkflowjava.workflow.engine.domain.chain.Node;
+
+public record NodeState(Node node, VariablePool variablePool, WorkflowMsgCallback callback) {
+    // 当前要执行的节点
+    // 变量池，用于存储和获取节点之间传递的数据
+    // 回调接口，用来把执行过程里的状态变化、输出内容实时传递出去
+}
---
+++ core-workflow-java/src/main/java/com/lyhn/coreworkflowjava/workflow/engine/domain/WorkflowDSL.java
@@ -0,0 +1,37 @@
+package com.lyhn.coreworkflowjava.workflow.engine.domain;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class WorkflowDSL {
+    /**
+     * Workflow ID
+     */
+    private String flowId;
+
+    /**
+     * Workflow UUID
+     */
+    private String uuid;
+
+    /**
+     * List of nodes in the workflow
+     */
+    @JsonProperty("nodes")
+    private List<Node> nodes = new ArrayList<>();
+
+    /**
+     * List of edges connecting nodes
+     */
+    @JsonProperty("edges")
+    private List<Edge> edges = new ArrayList<>();
+
+
+}
