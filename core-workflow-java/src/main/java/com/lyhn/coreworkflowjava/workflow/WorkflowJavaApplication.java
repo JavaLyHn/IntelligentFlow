a878620f02f1ac1dcbff8baa5ffe8acae0352d96
JavaLyHn <2443560753@qq.com>
2026-03-08 15:26:26 +0800

    异常类的编写

--- core-workflow-java/src/main/java/com/lyhn/coreworkflowjava/workflow/WorkflowJavaApplication.java
+++ core-workflow-java/src/main/java/com/lyhn/coreworkflowjava/workflow/WorkflowJavaApplication.java
@@ -1,4 +1,4 @@
-package com.lyhn.coreworkflowjava;
+package com.lyhn.coreworkflowjava.workflow;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
