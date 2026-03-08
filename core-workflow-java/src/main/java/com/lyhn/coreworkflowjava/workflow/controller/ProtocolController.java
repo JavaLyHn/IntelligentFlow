145d082843385603e891cd022dfe0d7950d657f2
JavaLyHn <2443560753@qq.com>
2026-03-08 18:30:14 +0800

    controller层开发

---
+++ core-workflow-java/src/main/java/com/lyhn/coreworkflowjava/workflow/controller/ProtocolController.java
@@ -0,0 +1,12 @@
+package com.lyhn.coreworkflowjava.workflow.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequestMapping("/workflow/v1/protocol")
+public class ProtocolController {
+
+}
